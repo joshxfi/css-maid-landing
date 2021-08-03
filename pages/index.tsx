5c023bc645a3529862c169554dcf520709e82493
Josh Daniel <joshpersonal8@gmail.com>
2021-08-03 12:17:54 +0800

    update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
+import Image from "next/image";
+import logo from "../public/img/logo.png";
 
 const index: React.FC = () => {
   return (
     <div className='w-9/12 mx-auto font-pops'>
       <Navbar />
       <main>
-        <div className='flex justify-between items-center h-screen'>
-          <div>
+        <div className='flex justify-between relative mt-28'>
+          <div className='pt-14'>
             <h1 className='text-5xl font-bold text-blue-700'>
               CSS Reset for your <br /> React App.
             </h1>
             <p className='font-medium'>get started!</p>
+          </div>
+          <div className='h-96 w-80 relative border-b-8 border-blue-700'>
+            <Image layout='fill' objectFit='cover' objectPosition='center' src={logo} alt='css maid logo' />
           </div>
         </div>
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pops: "Poppins, sans-serif",
+      },
+    },
   },
   variants: {
     extend: {},
